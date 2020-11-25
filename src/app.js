97e9ea6acf3954a29a93b1b09f6de2c4ac7dcfde
Mia-Yang <siyuyang1995@gmail.com>
2020-11-25 18:13:05 +0800

    [yang siyu]feat:  could delete, edit and toggle item

--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
 // save and get history from localStorage
-const saveListToLocalStorage = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
+const saveListToLocalStorage = (value) => {
+    localStorage.setItem("my-todoList", JSON.stringify(value));
 }
 
-const getPersistedListFromLocalStorage = (key) => {
-    if (localStorage.getItem(key) !== null) {
-        return JSON.parse(localStorage.getItem(key));
+const getPersistedListFromLocalStorage = () => {
+    if (localStorage.getItem("my-todoList") !== null) {
+        return JSON.parse(localStorage.getItem("my-todoList"));
     }
     return [];
 }
 
 // initialize history
 const initialize = () => {
-    getPersistedListFromLocalStorage("my-todoList").forEach(item => renderItem(item))
+    getPersistedListFromLocalStorage().forEach(item => renderItem(item))
 }
 window.addEventListener('load', initialize)
 
 // add todos
-const addItemIntoTodos = (item) => {
-    const todos = getPersistedListFromLocalStorage("my-todoList");
-    let newTodos = [...todos, item]
-    saveListToLocalStorage("my-todoList", newTodos);
+const addItemIntoStorage = (item) => {
+    let newTodos = [...getPersistedListFromLocalStorage(), item]
+    saveListToLocalStorage(newTodos);
 }
 
 const todoList = document.querySelector(".todo-list");
@@ -30,11 +29,12 @@ addButton.addEventListener('click', addItem)
 
 function renderItem(item) {
     const todoItem = document.createElement("li");
+    todoItem.setAttribute("id", item.id)
     let isChecked = item.completed ? "checked" : "";
 
     todoItem.innerHTML =
         `<input type="checkbox" onclick="toggleTodo(${item.id})" ${isChecked}> 
-    <span contenteditable id="text-${item.id}"> ${item.text} </span>
+    <span id="text-${item.id}" onfocus="editTodo(${item.id})" class="single-line" contenteditable > ${item.text} </span>
     <button class="del" onclick="removeTodo(${item.id})">✖️</button>`;
     todoList.appendChild(todoItem);
 }
@@ -50,13 +50,42 @@ function addItem(e) {
     }
     if (textContent.length !== 0) {
         renderItem(newTodo);
-        addItemIntoTodos(newTodo);
+        addItemIntoStorage(newTodo);
     }
 }
 
 //delete todos
+window.removeTodo = removeTodo;
+
+function removeTodo(id) {
+    saveListToLocalStorage(getPersistedListFromLocalStorage().filter(todo => todo.id !== id));
+    todoList.removeChild(document.getElementById(id));
+}
 
 //toggle todos
+window.toggleTodo = toggleTodo;
+
+function toggleTodo(id) {
+    saveListToLocalStorage(getPersistedListFromLocalStorage().map(item => item.id === id ? {...item, completed: !item.completed } : item));
+    document.getElementById(id).classList.toggle("finished")
+}
+
+//edit todos
+window.editTodo = editTodo;
+
+function editTodo(id) {
+    const textSpan = document.getElementById("text-" + id);
+    const originalText = textSpan.innerText;
+    textSpan.addEventListener('blur', function() {
+        const newText = textSpan.innerText
+        if (newText.trim().length) {
+            const editedTodo = getPersistedListFromLocalStorage().map(item => item.id === id ? {...item, text: newText.trim() } : item);
+            saveListToLocalStorage(editedTodo);
+        } else {
+            textSpan.innerText = originalText;
+        }
+    })
+}
 
 //clear todos
 const clearButton = document.querySelector(".clear")
